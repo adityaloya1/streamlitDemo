f50360717be541d520c918db9d4889e25731bec6
Aditya Narayan Loya <65150148+adityaloya1@users.noreply.github.com>
2024-04-21 22:27:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -169,9 +169,9 @@ else:
     if st.session_state.data_uploaded_check:
         pass
     else:
-        st.session_state.df_house = pd.read_csv('400_households.csv')
-        st.session_state.df_product = pd.read_csv('400_products.csv')
-        st.session_state.df_transaction = pd.read_csv('400_transactions.csv')
+        st.session_state.df_house = pd.read_csv('s3://400_households.csv')
+        st.session_state.df_product = pd.read_csv('s3://400_products.csv')
+        st.session_state.df_transaction = pd.read_csv('s3://400_transactions.csv')
 
         st.session_state.df_transaction_house = pd.merge(st.session_state.df_transaction, st.session_state.df_house, on='HSHD_NUM')
         st.session_state.df_thp = pd.merge(st.session_state.df_transaction_house, st.session_state.df_product, on='PRODUCT_NUM')
